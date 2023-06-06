1987e780ab52419c0c13b14f11c3ffd86c7792a9
salmehd <hormortorshor1@gmail.com>
2023-06-06 14:17:23 -0700

    python

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for letter in range(97, 123):
+    if chr(letter) != 'e' and chr(letter) != 'q':
+        print("{}".format(str(chr(letter))), end="")
+    else:
+        continue
